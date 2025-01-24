1e3acac99f86ad2e16d43a2e6065752b850cb81c
bluebread <hotbread70127@gamil.com>
2025-01-24 08:53:57 +0000

    block-wide scan algorithm completed

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC=g++
 NVCC=nvcc
 CXXFLAGS= -Xcompiler -fopenmp,-Wextra
-CUDAFLAGS= -g -O3 --generate-line-info -lcublas -arch=sm_89 -diag-suppress 177
+CUDAFLAGS= -g -O3 --generate-line-info -lcublas -arch=sm_89 -diag-suppress=177
 
 SRC_DIR := src
 BIN_DIR := bin
--- src/scan.cu
+++ src/scan.cu
@@ -81,11 +81,9 @@ __host__ void test_scan_openmp(const data_t * X_h, data_t * Y_h, int N) {
 }
 
 __host__ void test_memcpy_d2d(const data_t * X_h, const data_t * ans_h, int N) {
-    data_t * X_d, * Y_d, * ans_d;
-    // cudaMalloc(&X_d, N * sizeof(data_t));
+    data_t * Y_d, * ans_d;
     cudaMalloc(&Y_d, N * sizeof(data_t));
     cudaMalloc(&ans_d, N * sizeof(data_t));
-    // cudaMemcpy(X_d, X_h, N * sizeof(data_t), cudaMemcpyHostToDevice);
     cudaMemcpy(ans_d, ans_h, N * sizeof(data_t), cudaMemcpyHostToDevice);
 
     cudaEventRecord(start);
@@ -103,7 +101,6 @@ __host__ void test_memcpy_d2d(const data_t * X_h, const data_t * ans_h, int N) {
     std::cout << "- Throughput (memcpy D2D): " << throughput << " GB/s"
         << " [" << is_correct_s << ", time: " << t << " ms]\n"; 
 
-    // cudaFree(X_d);
     cudaFree(Y_d);
     cudaFree(ans_d);
 }
@@ -150,9 +147,12 @@ __global__ void scan_d_kernel(
     AggregateDescriptor *       aggregate_descriptor_table,
     InclusivePrefixDescriptor * inclusive_prefix_descriptor_table) {
         
-    __shared__ data_t Y_s[warpSize * warpSize];
-    __shared__ data_t sdata[warpSize];
+    __shared__ data_t Y_s[1024];
+    __shared__ data_t sdata[32];
     __shared__ unsigned int bid_s;
+
+    const int warpIdx = threadIdx.x / warpSize;
+    const int laneIdx = threadIdx.x % warpSize;
 
     int base, len;
 
@@ -160,7 +160,7 @@ __global__ void scan_d_kernel(
         /* Dynamic block index assignment */
 
         if (threadIdx.x == 0) {
-            bid_s = atomicInc(block_counter, 1);
+            bid_s = atomicAdd(block_counter, 1);
         }
 
         __syncthreads();
@@ -174,18 +174,27 @@ __global__ void scan_d_kernel(
 
         /* The Kogge-Stone algorithm (Warp level) */
 
+        data_t aggregate = 0;
+
         for (int i = threadIdx.x; i < warpSize * warpSize; i += blockDim.x) {
-            data_t yi = (i < len) ? X[base + i] : 0;
+            data_t xi = (i < len) ? X[base + i] : 0;
+            data_t yi = xi;
+            data_t yi_prev = 0;
 
-            yi += __shfl_up_sync(__activemask(), yi, 1);
-            yi += __shfl_up_sync(__activemask(), yi, 2);
-            yi += __shfl_up_sync(__activemask(), yi, 4);
-            yi += __shfl_up_sync(__activemask(), yi, 8);
-            yi += __shfl_up_sync(__activemask(), yi, 16);
+            yi_prev = __shfl_up_sync(__activemask(), yi, 1);
+            yi += (laneIdx >= 1) ? yi_prev : 0;
+            yi_prev = __shfl_up_sync(__activemask(), yi, 2);
+            yi += (laneIdx >= 2) ? yi_prev : 0;
+            yi_prev = __shfl_up_sync(__activemask(), yi, 4);
+            yi += (laneIdx >= 4) ? yi_prev : 0;
+            yi_prev = __shfl_up_sync(__activemask(), yi, 8);
+            yi += (laneIdx >= 8) ? yi_prev : 0;
+            yi_prev = __shfl_up_sync(__activemask(), yi, 16);
+            yi += (laneIdx >= 16) ? yi_prev : 0;
 
-            Y_s[i] = yi;
+            Y_s[i] = yi - xi;
 
-            if (i % warpSize == warpSize - 1) {
+            if (laneIdx == warpSize - 1) {
                 sdata[i / warpSize] = yi;
             }
         }
@@ -195,23 +204,33 @@ __global__ void scan_d_kernel(
         /* Coarsened scan (Block level) */
 
         if (threadIdx.x < warpSize) {
-            data_t yi = sdata[threadIdx.x];
+            data_t xi = sdata[threadIdx.x];
+            data_t yi = xi;
+            data_t yi_prev = 0;
 
-            yi += __shfl_up_sync(__activemask(), yi, 1);
-            yi += __shfl_up_sync(__activemask(), yi, 2);
-            yi += __shfl_up_sync(__activemask(), yi, 4);
-            yi += __shfl_up_sync(__activemask(), yi, 8);
-            yi += __shfl_up_sync(__activemask(), yi, 16);
+            yi_prev = __shfl_up_sync(__activemask(), yi, 1);
+            yi += (laneIdx >= 1) ? yi_prev : 0;
+            yi_prev = __shfl_up_sync(__activemask(), yi, 2);
+            yi += (laneIdx >= 2) ? yi_prev : 0;
+            yi_prev = __shfl_up_sync(__activemask(), yi, 4);
+            yi += (laneIdx >= 4) ? yi_prev : 0;
+            yi_prev = __shfl_up_sync(__activemask(), yi, 8);
+            yi += (laneIdx >= 8) ? yi_prev : 0;
+            yi_prev = __shfl_up_sync(__activemask(), yi, 16);
+            yi += (laneIdx >= 16) ? yi_prev : 0;
 
-            sdata[threadIdx.x] = yi;
+            sdata[threadIdx.x] = yi - xi;
+            aggregate = yi;
         }
 
         __syncthreads();
 
         for (int i = threadIdx.x; i < len; i += blockDim.x) {
-            if (i >= warpSize) 
-                Y_s[i] += sdata[i / warpSize - 1];
+            Y_s[i] += sdata[i / warpSize];
+            Y[base + i] = Y_s[i]; // debug
         }
+
+        return;
 
         __syncthreads();
 
@@ -220,14 +239,16 @@ __global__ void scan_d_kernel(
 
         data_t pred_sum = 0;
 
-        if (threadIdx.x == 0) {
+        if (warpIdx == 0 && laneIdx == warpSize - 1) {
             union AggregateDescriptor aggr_desr;
 
             // Optimization: Fence-free descriptor update
             aggr_desr.flag = true;
-            aggr_desr.val = Y_s[len - 1];
+            aggr_desr.val = aggregate;
             aggregate_descriptor_table[bid_s].u64 = aggr_desr.u64;
         }
+
+        return;
 
         int inclpref_idx = -1;
 
@@ -267,7 +288,7 @@ __global__ void scan_d_kernel(
 
             // TODO: it is possible to have no predecessors with status "P". fix this.
             // Case 1: All predecessors have status "A"
-            if (inclpref_idx < 0) {
+            if (inclpref_idx < 0 && k - threadIdx.x > 0) {
                 // Since all predecessors have status "A", we can safely add the value 
                 // and continue to the next iteration. 
                 pred_sum += pred_ag.val;
@@ -296,6 +317,8 @@ __global__ void scan_d_kernel(
                 Y_s[i] += pred_sum;
                 Y[base + i] = Y_s[i];
             }
+
+            inclpref_idx = 0;
         }
         
         __syncthreads();
@@ -317,8 +340,8 @@ __host__ void malloc_persisting_data(T ** ptr_addr, unsigned int n, float hit_ra
     cudaMemset(*ptr_addr, 0, n * sizeof(T));
 
     cudaStreamAttrValue stream_attribute;   
-    stream_attribute.accessPolicyWindow.base_ptr  = reinterpret_cast<void*>(inclusive_prefix_descriptor_table_d);
-    stream_attribute.accessPolicyWindow.num_bytes = inclusive_prefix_descriptor_table_size;
+    stream_attribute.accessPolicyWindow.base_ptr  = reinterpret_cast<void*>(ptr_addr);
+    stream_attribute.accessPolicyWindow.num_bytes = n * sizeof(T);
     stream_attribute.accessPolicyWindow.hitRatio  = hit_ratio;
     stream_attribute.accessPolicyWindow.hitProp   = cudaAccessPropertyPersisting;
     stream_attribute.accessPolicyWindow.missProp  = cudaAccessPropertyStreaming;
@@ -337,7 +360,7 @@ __host__ void test_scan_handcraft(const data_t * X_h, const data_t * ans_h, int 
     cudaDeviceProp prop = utils::get_device_properties();
     int num_threads = 256; 
     int num_blocks = prop.maxBlocksPerMultiProcessor * prop.multiProcessorCount;
-    int partition_size = warpSize * warpSize;
+    int partition_size = prop.warpSize * prop.warpSize;
     int num_partitions = (N + partition_size - 1) / partition_size;
 
     unsigned int * block_counter_d;
@@ -364,6 +387,25 @@ __host__ void test_scan_handcraft(const data_t * X_h, const data_t * ans_h, int 
 
     float t;
     cudaEventElapsedTime(&t, start, stop);
+
+    // debug
+    data_t * Y_h = new data_t[N];
+    int * block_counter_h = new int[1];
+    cudaMemcpy(Y_h, Y_d, N * sizeof(data_t), cudaMemcpyDeviceToHost);
+    cudaMemcpy(block_counter_h, block_counter_d, sizeof(int), cudaMemcpyDeviceToHost);
+    printf("num_threads: %d\n", num_threads);
+    printf("num_blocks: %d\n", num_blocks);
+    printf("partition_size: %d\n", partition_size);
+    printf("num_partitions: %d\n", num_partitions);
+    printf("block_counter: %d\n", *block_counter_h);
+    for (int i = 0; i < 1024; i++) {
+        std::cout << Y_h[i] << " ";
+        if (i % 32 == 31) {
+            std::cout << std::endl;
+        }
+    }
+    std::cout << std::endl;
+
 
     const char * is_correct_s = check_results(Y_d, ans_d, N) ? "correct" : "incorrect";
     float throughput = (float)(N * sizeof(data_t) * 2) / (t * 1e-3) / 1e9;
@@ -409,12 +451,29 @@ __host__ int main(int argc, char ** argv) {
 
     data_t * X_h = new data_t[N];
     data_t * Y_h = new data_t[N];
-    utils::random_fill_h<data_t>(X_h, N, 0, 100); 
+    utils::random_fill_h<data_t>(X_h, N, 0, 10); 
+    test_scan_openmp(X_h, Y_h, N);
+
+    // for(int i = 0; i < 256; i++) {
+    //     std::cout << X_h[i] << " ";
+    //     if (i % 32 == 31) {
+    //         std::cout << std::endl;
+    //     }
+    // }
+    // std::cout << std::endl;
+    std::cout << "----------------------------------------" << std::endl;
+    for(int i = 0; i <  1024; i++) {
+        std::cout << Y_h[i] << " ";
+        if (i % 32 == 31) {
+            std::cout << std::endl;
+        }
+    }
+    std::cout << std::endl;
 
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
 
-    test_scan_openmp(X_h, Y_h, N);
+    // test_scan_openmp(X_h, Y_h, N);
     test_memcpy_d2d(X_h, Y_h, N);
     test_scan_cub(X_h, Y_h, N);
     test_scan_handcraft(X_h, Y_h, N);
